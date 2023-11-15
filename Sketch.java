6e8014c2b4d2c44657e05d174a9482819fec68a3
Ewu52 <ethan.wu25@ycdsbk12.ca>
2023-11-15 09:44:00 -0500

    draw images

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,78 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+	PImage imgMonastery;
+  PImage imgRowlet;
+
+  // Circle location and speeds
+  float floatCircleX = 200;
+  float floatCircleY = 300;
+  float floatCircleSpeedX = 5;
+  float floatCircleSpeedY = -5;
+
+  // Rowlet location and speeds
+  float floatRowletX = -5;
+  float floatRowletY = -50;
+  float floatRowletSpeedX = 5;
+  float floatRowletSpeedY = 0;
+  
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(1600, 800);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  // Sets up the Background
   public void setup() {
-    background(210, 255, 173);
+    imgMonastery = loadImage("garreg mach monastery.png");
+    imgRowlet = loadImage("rowlet-removebg-preview.png");
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	   
+    // Draw the Background
+    image(imgMonastery, -500, -500);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Draw Rowlet
+    image(imgRowlet, floatRowletX, floatRowletY);
+
+    // Update Rowlet's position
+    floatRowletX = floatRowletX + floatRowletSpeedX;
+
+    // Basic Edge Detection and Switch Direction when ROWLET hits the border.
+    if(floatRowletX < 0) {
+      floatRowletSpeedX = floatRowletSpeedX * -1;
+    }
+    else if(floatRowletX > 1600 - 550) {
+      floatRowletSpeedX = floatRowletSpeedX * -1;
+    }
+
+    // Print Circle
+    fill(238, 10, 158);
+    ellipse(floatCircleX, floatCircleY, 200, 200);
+
+    // Update Circle's position
+    floatCircleX = floatCircleX + floatCircleSpeedX;
+    floatCircleY = floatCircleY + floatCircleSpeedY;
+
+    // Basic Edge Detection and Switch Direction when CIRCLE hits the border.
+    if(floatCircleX < 0) {
+      floatCircleX = floatCircleX * -1;
+    }
+    else if(floatCircleX > width - 200) {
+      floatCircleX = floatCircleX * -1;
+    }
+
+    if(floatCircleY < 100) {
+      floatCircleY = floatCircleY * -1;
+    }
+    else if(floatCircleY > height + 200) {
+      floatCircleY = floatCircleY * -1;
+    }
   }
+
+    
   
-  // define other methods down here.
+  
 }
